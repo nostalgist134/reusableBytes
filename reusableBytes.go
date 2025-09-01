459676dfc171b86de0d39561ee85f1a12054d2c9
nostalgist134 <CAAA3333@126.com>
2025-09-01 19:46:30 +0800

    update

--- reusableBytes.go
+++ reusableBytes.go
@@ -50,6 +50,11 @@ func (rb *ReusableBytes) WriteString(s string) int {
 	return len(s)
 }
 
+// Len 获取当前缓冲区的长度
+func (rb *ReusableBytes) Len() int {
+	return rb.cursor
+}
+
 // Reset 重置缓冲区
 func (rb *ReusableBytes) Reset() {
 	rb.cursor = 0
